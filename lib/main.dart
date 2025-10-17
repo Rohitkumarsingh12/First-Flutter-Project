9b46bfb54df32012debe7422c567ebfcbe43abd7
Rohitkumarsingh12 <153908585+Rohitkumarsingh12@users.noreply.github.com>
2025-10-17 15:26:36 +0530

    Add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:first_git_project/home_screen.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+   Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
